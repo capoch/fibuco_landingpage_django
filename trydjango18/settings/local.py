10124655e2c5c70923a141663cf23c49c06929a2
capoch <buemplizer@gmail.com>
2017-01-22 00:03:51 +0800

    trydjango18 final

--- trydjango18/settings/local.py
+++ trydjango18/settings/local.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = '9iz^ef0u!tj7+0e6*ii5rwd-a+yv6m^l(kdnni&lx(xp@2t)sa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 EMAIL_HOST = 'smtp.gmail.com'
